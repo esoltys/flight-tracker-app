c68c37fe3289541ac333a00f1caf27ce46c257e5
Eric James Soltys <ericjamessoltys@outlook.com>
2024-09-11 16:46:13 -0700

    update loading animation

--- src/components/App.vue
+++ src/components/App.vue
@@ -42,19 +42,17 @@
     </div>
 
     <div v-if="isLoading" class="flex justify-center items-center my-8">
-      <svg class="animate-spin h-20 w-20 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none"
-        viewBox="0 0 24 24">
-        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-        <path class="opacity-75" fill="currentColor"
-          d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
-        </path>
-        <path fill="currentColor" d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5-10-5-10 5z" />
-        <path fill="currentColor" d="M12 22.5L2 17.5V7.5l10 5v10z" />
-        <path fill="currentColor" d="M12 22.5l10-5V7.5l-10 5v10z" />
-        <path fill="currentColor" d="M12 12l-10-5 10-5 10 5-10 5z" />
+      <svg class="h-40 w-40" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="50" cy="50" r="40" fill="none" stroke="#e0e0e0" stroke-width="2" />
+        <path id="motionPath" d="M50,10 A40,40 0 1,1 50,90 A40,40 0 1,1 50,10" fill="none" />
+        <g class="airplane">
+          <path fill="#3b82f6" d="M-15,-6 L15,0 L-15,6 L-9,0 Z" />
+          <animateMotion dur="3s" repeatCount="indefinite" rotate="auto">
+            <mpath href="#motionPath" />
+          </animateMotion>
+        </g>
       </svg>
     </div>
-
 
     <div v-if="!isLoading && flights.length > 0" class="mt-6">
       <div v-for="(flight, index) in flights" :key="index" class="bg-white shadow-md rounded-lg overflow-hidden mb-4">
