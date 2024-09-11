a76d06888daed377579cd18e89fd79d698ea26c1
Eric James Soltys <ericjamessoltys@outlook.com>
2024-09-11 15:37:07 -0700

    update Flight Number placeholder

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
               d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
           </svg>
         </span>
-        <input v-model="flightNumber" placeholder="WS3041" class="pl-10 pr-4 py-2 w-full border rounded-md">
+        <input v-model="flightNumber" placeholder="Flight Number (e.g. WS3041)" class="pl-10 pr-4 py-2 w-full border rounded-md">
       </div>
       <div class="flex-1 relative">
         <span class="absolute inset-y-0 left-0 flex items-center pl-3">
